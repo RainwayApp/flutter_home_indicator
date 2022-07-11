ff98a8ef3b1fe5032398256e5acaae6e80583f41
nohli <43643339+nohli@users.noreply.github.com>
2022-07-11 22:50:17 +0200

    Update HomeIndicatorPlugin.m

--- ios/Classes/HomeIndicatorPlugin.m
+++ ios/Classes/HomeIndicatorPlugin.m
@@ -1,5 +1,12 @@
 #import "HomeIndicatorPlugin.h"
-#import <home_indicator-Swift.h>
+#if __has_include(<home_indicator/home_indicator-Swift.h>)
+#import <home_indicator/home_indicator-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "home_indicator-Swift.h"
+#endif
 
 @implementation HomeIndicatorPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
